ccb41cb1ec7062e1d4698fbfe1637fdb42687f7c
alexandre richard <alexandre.richard49410@gmail.com>
2024-04-08 23:07:43 +0200

    mysql

--- config/database.ts
+++ config/database.ts
@@ -1,18 +1,20 @@
-import app from '@adonisjs/core/services/app'
+{{{
+  exports({ to: app.configPath('database.ts') })
+}}}
+import env from '#start/env'
 import { defineConfig } from '@adonisjs/lucid'
-import { env } from 'process'
 
 const dbConfig = defineConfig({
   connection: 'mysql',
   connections: {
     mysql: {
-      client: 'mysql',
+      client: 'mysql2',
       connection: {
-        host: env.DB_HOST,
-        port: 0,
-        user: env.DB_USER,
-        password: env.DB_PASSWORD,
-        database: env.DB_DATABASE,
+        host: env.get('DB_HOST'),
+        port: env.get('DB_PORT'),
+        user: env.get('DB_USER'),
+        password: env.get('DB_PASSWORD'),
+        database: env.get('DB_DATABASE'),
       },
       migrations: {
         naturalSort: true,
